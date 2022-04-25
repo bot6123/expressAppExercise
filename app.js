0492b7360a17ccb8686a9bb51cd5cd5a0d162185
bot6123 <101458788+bot6123@users.noreply.github.com>
2022-04-25 11:50:00 -0400

    Setup stock routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+var stocksRouter = require('./routes/stocks')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/stocks', stocksRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
